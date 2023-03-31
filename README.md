fc0169db08007cedca2c1e43cf9063d9695df0e6
pratham-404 <prathamloya12345@gmail.com>
2023-04-01 05:20:05 +0530

    code update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # limnux
 My goto Script to get my Linux setup on any PC.
+
+```bash
+    git clone https://github.com/pratham-404/limnux.git
+    cd limnux
+    sudo ./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,8 +1,29 @@
+sudo apt install neofetch screenfetch hollywood 
+
+sudo apt install git nodejs npm code
+sudo snap install discord
+
+sudo apt install mysql-server mongodb
+
+sudo apt-get install spotify-client
+
 sudo apt-get install g++ gcc python3 
 sudo apt install default-jre default-jdk
 sudo update-alternatives --config java
 
-cd files
+pip install numpy pandas scikit-learn matplotlib seaborn tensorflow
+
+# starship prompt
+curl -sS https://starship.rs/install.sh | sh
+
+# pywal
+sudo apt install imagemagick python3-pip
+sudo pip3 install pywal
+
+# motivate
+git clone https://github.com/mubaris/motivate.git ; cd motivate/motivate ; sudo ./install.sh ; source ~/.bashrc
+
+cd hello
 
 gcc hello.c -o helloc.out
 ./helloc.out
@@ -14,3 +35,10 @@ javac hello.java
 java hello
 
 python3 hello.py
+
+cd ..
+
+cp -R ./bootloader ~/.bootloader
+cp -R ./jarvis ~/.jarvis
+
+sudo cat ./bash.txt >> ~/.bashrc
